4c045f7003bfe60045e0abe2eb42595ab80b36bd
Muammar El Khatib <muammarelkhatib@gmail.com>
2018-01-22 13:49:46 -0500

    Initial commit.

---
+++ restaurants.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+groups = [
+        'Chloe',
+        "Antonio's",
+        'Mikes',
+        'Korean Fried Chicken',
+        ]
+
+iteration = 0
+recurrence = 0
+
+while True:
+    iteration += 1
+    import random
+    secure_random = random.SystemRandom()
+
+    a = secure_random.choice(groups)
+    b = random.choice(groups)
+    c = random.choice(groups)
+    d = secure_random.choice(groups)
+    e = random.choice(groups)
+    f = secure_random.choice(groups)
+
+    if a == b == c == d == e == f:
+        recurrence += 1
+        if recurrence == 4:
+            print('After %s iterations, group chosen is %s.' % (iteration, a))
+            break
